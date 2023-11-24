972886e880a5a9f80c2f799c2fb6d5dcdb0f2fde
CodyMarkix <Markix124@protonmail.com>
2023-11-24 08:54:06 +0100

    work on css

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const child_process = require('child_process');
 const path = require('path');
+const { platform } = require('os');
 
 const app = express();
 const port = 8080;
@@ -16,8 +18,13 @@ app.get('/javascript', (req, res) => {
     res.sendFile(`${__dirname}/javascript.html`);
 });
 
+app.get('/css', (req, res) => {
+    res.sendFile(`${__dirname}/css.html`);
+});
+
 app.use('/public', express.static(`${__dirname}/../public`))
 
 app.listen(port, () => {
     console.log(`Now serving website at port ${port}!\nLocal: http://127.0.0.1:8080`);
+    // let browserWindow = process.platform == "win32" ? child_process.spawn(`explorer http://localhost:${port}`) : child_process.spawn(`open http://localhost:${port}`) 
 });
