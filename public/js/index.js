a8bafd86b2b0149d8f022418160ce631c1463e56
CodyMarkix <Markix124@protonmail.com>
2024-01-05 09:52:07 +0100

    reworked autoscroll to include a confirm button
    it's also broken

--- public/js/index.js
+++ public/js/index.js
@@ -1,23 +1,60 @@
-function switchToNextRoute() {
+let hasCreatedContinueButton = false;
+
+function toggleButton() {
     const currentLocation = document.location;
     let html = document.querySelector('html'); 
 
     switch(currentLocation.pathname) {
         case "/":
             if (html.scrollTop > 3800) {
-                window.scrollTo(0, 0);
-                window.location.href = `${currentLocation.origin}/css`
+                // window.scrollTo(0, 0);
+                // window.location.href = `${currentLocation.origin}/css`
+                createContinueButton();
             }
 
         case "/css":
             if (html.scrollTop > 3400) {
-                window.scrollTo(0, 0);
-                window.location.href = `${currentLocation.origin}/javascript`
+                // window.scrollTo(0, 0);
+                // window.location.href = `${currentLocation.origin}/javascript`
+                createContinueButton();
             }
+    }
+}
+
+function createContinueButton() {
+    if (!hasCreatedContinueButton) {
+        hasCreatedContinueButton = true;
+
+        const documentDiv = document.querySelector('#congratulations');
+
+        let continueDiv = document.createElement('div');
+        let continueButton = document.createElement('input');
+        
+        continueDiv.id = "continue-button";
+        continueButton.value = "Pokračovat?";
+        continueButton.type = "button";
+        continueButton.style.opacity = 0.0;
+        continueButton.style.animation = "fadeIn 1s 1s forwards";
+        continueButton.onclick = function() { onContinueClick(); };
+        continueDiv.appendChild(continueButton);
+    
+        documentDiv.appendChild(continueDiv);
     }
 }
 
 let interval = 7 // basically just for convienience
 setInterval(() => {
-    switchToNextRoute();
+    toggleButton();
 }, 7 * 1000);
+
+function onContinueClick() {
+    switch(currentLocation.pathname) {
+        case "/":
+            window.scrollTo(0, 0);
+            window.location.href = `${currentLocation.origin}/css`
+
+        case "/css":
+            window.scrollTo(0, 0);
+            window.location.href = `${currentLocation.origin}/javascript`
+    }
+}
